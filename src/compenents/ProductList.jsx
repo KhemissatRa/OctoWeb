3299da409a529c5eeeb721b50aaa496d7a8d8596
khemissat <khemissat.ra@gmail.com>
2025-12-21 20:39:52 +0100

    change langauge

--- src/compenents/ProductList.jsx
+++ src/compenents/ProductList.jsx
@@ -38,8 +38,8 @@ export default function ProductList() {
             to={`productDetais/${product._id}`}
             className="mt-auto inline-flex items-center justify-center px-4 py-2 rounded-lg bg-indigo-600 text-white font-medium hover:bg-indigo-700 transition-colors duration-300"
           >
-            Buy Now
-          </Link>
+                اشتري الان
+           </Link>
         </div>
 
       </div>
